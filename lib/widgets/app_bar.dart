8aa090c1bcc7f23023488540ea6ecf82f6942db0
abdalazeezS <191021@ppu.edu.ps>
2022-10-30 15:40:09 +0200

    extract appBar as widget

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+  final String title;
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      foregroundColor: Colors.black,
+      elevation: 0,
+      title: Text(widget.title),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.more_vert),
+        ),
+      ],
+    );
+  }
+}
